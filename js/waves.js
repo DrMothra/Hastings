e3a5527f8713984b5e9d5c11b37b6cfdb816df23
Tony Glover <tony.glover@nottingham.ac.uk>
2015-04-09 01:38:09 +0100

    Integrated pubnub

--- js/waves.js
+++ js/waves.js
@@ -10,14 +10,27 @@ SmoothApp.prototype = new BaseSmoothApp();
 
 SmoothApp.prototype.init = function() {
     //Initialise app
+    this.data = null;
+    this.channelName = 'hastings';
+    this.subscribe();
     BaseSmoothApp.prototype.init.call(this);
+};
+
+SmoothApp.prototype.subscribe = function() {
+    //Subscribe to pubnub channel
+    this.channel = PubNubBuffer.subscribe(this.channelName,
+        "sub-c-2eafcf66-c636-11e3-8dcd-02ee2ddab7fe",
+        1000,
+        300);
 };
 
 SmoothApp.prototype.update = function() {
     //Perform any updates
-    time += 16;
-    for(var i=0; i<this.smoothies.length; ++i) {
-        this.timeSeries[i].append(new Date().getTime(), Math.random());
+    this.data = this.channel.getLastValue('wave');
+    if(this.data != undefined && this.data > 10) {
+        for(var i=0; i<this.smoothies.length; ++i) {
+            this.timeSeries[i].append(new Date().getTime(), this.data);
+        }
     }
 };
 
